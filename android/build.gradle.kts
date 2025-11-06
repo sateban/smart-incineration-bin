a6de4f6cdfc264da38ca6bd22b9ae9b31c40d118
stevenjake72898 <fajarillostevenjake@gmail.com>
2025-11-06 21:55:31 +0800

    feature: realtime firebase communication setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,9 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
